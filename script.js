15f6ce2b7366adaea2f00a62b3fcb8e2082184ba
imollov <ivomollov@gmail.com>
2020-10-17 17:28:35 +0300

    Toggle mobile nav

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-window.alert("Hello world");
+/*
+* Toggle mobile nav
+*/ 
+(function () {
+  var toggleButton = document.getElementById("toggle-button");
+  var mobileNav = document.querySelector(".mobile-nav");
+
+  toggleButton.addEventListener("click", toggle);
+
+  function toggle() {
+    if (!toggleButton.classList.contains("open")) {
+      open();
+      toggleButton.classList.add("open");
+    } else {
+      close();
+      toggleButton.classList.remove("open");
+    }
+  }
+
+  function open() {
+    mobileNav.style.display = "block";
+    setTimeout(function () {
+      mobileNav.classList.add("open");
+      document.body.classList.add("nav-open");
+    }, 10);
+  }
+
+  function close() {
+    mobileNav.classList.remove("open");
+    document.body.classList.remove("nav-open");
+    setTimeout(function () {
+      mobileNav.style.display = "none";
+    }, 250);
+  }
+})();
